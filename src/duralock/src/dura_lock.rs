e0aeb3cf6e9ffaef69665f3f5afb663531cc851d
pepsican <sekhar@baskcart.com>
2022-06-21 22:42:47 -0400

    added interface

---
+++ src/duralock/src/dura_lock.rs
@@ -0,0 +1,139 @@
+const  DURATION_EXTRACT: u64 = 100000000; //3 digit days// 2 digit hours// 2 digit minutes
+
+use std::collections::HashMap;
+use std::collections::BTreeSet;
+use std::collections::hash_map::DefaultHasher;
+use candid::CandidType;
+use oorandom::Rand64;
+use std::hash::Hash;
+use std::hash::Hasher;
+use chrono::{DateTime,Duration,Utc, NaiveDateTime};
+
+const  SOME_SEED:u128 = 4;
+lazy_static! {
+    static ref RANDOM_GEN:Rand64 =   {
+        let m =oorandom::Rand64::new(SOME_SEED);
+        m
+    };
+}
+
+
+#[derive(Default,Eq,  PartialEq,  Clone)]
+pub struct DuraKey{
+    dura_slot: DuraSlot,
+    key_hash: u64
+}
+impl Hash for DuraKey {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.dura_slot.hash(state);
+        self.key_hash.hash(state);
+    }
+}
+
+impl DuraKey {
+
+    pub fn generate_key(&mut self)  -> u64{
+       let random_key = 0 as u64;
+      // let random_key = RANDOM_GEN.rand_u64();
+       let mut s = DefaultHasher::new();
+       random_key.hash(&mut s);
+       self.key_hash = s.finish();
+       return random_key;
+    }
+
+    pub fn get_key_hash(&self) -> u64 {
+        return self.key_hash;
+    }
+}
+
+#[derive(Default,Eq, PartialEq,  Clone)]
+pub struct DuraSlot {
+    time_and_duration: u64
+}
+
+impl Hash for DuraSlot{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.time_and_duration.hash(state);
+    }
+}
+
+impl  DuraSlot {
+    pub fn init (td: u64) -> Self {
+       Self { time_and_duration: td }
+    }
+
+    pub fn new (st: String, et: String) -> Self {
+        Self { time_and_duration: 0 }
+    }
+
+    pub fn now_in_this(&self) -> bool {
+        let time_now = Utc::now();
+        let start_time = self.time_and_duration/ DURATION_EXTRACT;
+        let duration: u64 = self.time_and_duration - start_time;
+
+        let start_date_time = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(start_time as i64, 0), Utc);
+        let end_date_time = start_date_time.checked_add_signed(Duration::seconds(duration as i64)).unwrap();
+     
+        if time_now>start_date_time&&time_now < end_date_time {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }    
+}
+#[derive(Default,Eq, Hash, PartialEq,  Clone)]
+pub struct AssetKey {
+    asset_name: String,
+    asset_id: u64
+}
+
+impl AssetKey {
+    pub fn new (name:String, id:u64) -> Self {
+        Self {
+            asset_name: name,
+            asset_id:id
+        }
+    }
+}
+
+
+#[derive(Default)]
+pub struct DuraLockDB {
+     asset_locks: HashMap<AssetKey,BTreeSet<DuraKey>>
+}
+
+
+impl DuraLockDB {
+
+     fn new() -> Self {
+        Self { 
+           asset_locks:HashMap::default()
+        }
+    }
+    pub fn get_key_hash(&self,name: &String) -> u64{
+        let asset_key = AssetKey::new(name.to_string(),0);
+        let mut return_val: u64=0;
+        if let Some(time_slots) =self.asset_locks.get(&asset_key) {
+            for dura_key  in time_slots.iter() {
+                if dura_key.dura_slot.now_in_this() {
+                    return_val = dura_key.key_hash;
+                }
+            }
+        }
+        return_val
+    }
+
+     pub fn generate_lock(&self, name:&String, start_date_time:&String, end_date_time:&String) -> String {
+       // let lk :DuraKey = DuraKey { duraSlot: startTime, keyHash: endTime };
+       // self.assetLocks.insert(123, lk);
+        String::from("done")
+    } 
+
+    pub fn clean(&self) {
+
+    } 
+
+}
+
+    
--- src/duralock/src/lib.rs
+++ src/duralock/src/lib.rs
@@ -1,4 +1,48 @@
+#[macro_use]
+extern crate lazy_static;
+
+mod dura_lock;
+use dura_lock::DuraLockDB;
+use ic_cdk_macros::{pre_upgrade, post_upgrade}; 
+use std::{cell::RefCell, borrow::Borrow};
+
+thread_local! {
+static DURA_LOCK_DB: RefCell<DuraLockDB> = RefCell::default();
+}
+
+#[ic_cdk_macros::update]
+fn gen_lock(name: String,start_date_time:String,end_date_time:String) -> String {
+let mut key_ret = "done".to_string();
+DURA_LOCK_DB.with(|db_ref| {
+    let db_mut = db_ref.borrow_mut();
+    key_ret = db_mut.generate_lock(&name,&start_date_time,&end_date_time);
+});
+format!("genLock, {} {} {} {}!", name,start_date_time,end_date_time,key_ret)
+   
+}
+
+
 #[ic_cdk_macros::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn get_key(name: String) -> String {
+    let mut key_hash_ret: u64 = 0;
+    DURA_LOCK_DB.with(|db_ref| {
+        let db_ref = db_ref.borrow();
+        key_hash_ret = db_ref.get_key_hash(&name);
+    });
+   
+    format!("getKey, {}{}!", name,key_hash_ret)
+}
+
+#[pre_upgrade]
+fn pre_upgrade() {
+    DURA_LOCK_DB.with(|db_ref| {
+        let db_mut = db_ref.borrow_mut();
+        db_mut.clean();
+      //  ic_cdk::storage::stable_save(db_mut);
+    }); 
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+
 }
